07b1cbb880c9cdf12f0f097831c88d5a9c3691c3
adhfarg <153331802+adhfarg@users.noreply.github.com>
2024-01-12 12:36:24 -0500

    ”completed”

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+
+  function fetchBooks() {
+    fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => renderBooks(data))
+      .catch(error => console.error(error));
+  }
+  function fetchBooks() {
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data => renderBooks(data))
+      .catch(error => console.error(error));
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
